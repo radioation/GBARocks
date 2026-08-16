58e39c7060e33662ba8f28aa7b081c54cc966fad
Greg Gallardo <greggallardo@gmail.com>
2026-08-16 15:30:28 -0500

    adding sprite

--- 06-scrolling_background/source/main.c
+++ 06-scrolling_background/source/main.c
@@ -5,6 +5,7 @@
 #include <gba_systemcalls.h>
 #include <gba_input.h>
 #include <gba_dma.h>
+#include <gba_sprites.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,8 +13,10 @@
 #include "myfix.h"
 
 #include "space.h"
-extern const  u16 map[1024];
+#include "ship.h"
 
+extern const  u16 map[1024];
+#define OAM_MEM ((volatile OBJATTR *)0x07000000)
 //---------------------------------------------------------------------------------
 // Program entry point
 //---------------------------------------------------------------------------------
@@ -28,9 +31,9 @@ int main(void) {
 	irqEnable(IRQ_VBLANK);
 
 	//SetMode( MODE_0 | BG1_ON );  // 4 backgoroudns, using BG2
-        REG_DISPCNT = ( MODE_0|BG1_ON );
+	REG_DISPCNT = ( MODE_0|BG1_ON | OBJ_ENABLE | OBJ_1D_MAP );
 
-	
+
 	//memcpy( BG_PALETTE, spacePal, spacePalLen );
 	dmaCopy( spacePal, BG_PALETTE, spacePalLen );
 
@@ -43,15 +46,44 @@ int main(void) {
 
 
 
+	// sprites
+	SPRITE_PALETTE[0] = RGB8( 255, 0, 0 );
+	SPRITE_PALETTE[1] = RGB8( 255, 255, 255 );
+	SPRITE_PALETTE[2] = RGB8( 218, 218, 218 );
+	SPRITE_PALETTE[3] = RGB8( 182, 182, 182 );
+	SPRITE_PALETTE[4] = RGB8( 145, 145, 145 );
+	SPRITE_PALETTE[5] = RGB8( 109, 109, 109 );
+	SPRITE_PALETTE[6] = RGB8( 72, 72, 72 );
+	SPRITE_PALETTE[7] = RGB8( 36, 36, 36 );
+	SPRITE_PALETTE[8] = RGB8( 0, 0, 0 );
+	SPRITE_PALETTE[9] = RGB8( 0, 109, 218 );
+	SPRITE_PALETTE[10] = RGB8( 0, 72, 109 );
+	SPRITE_PALETTE[11] = RGB8( 0, 36, 72 );
+	SPRITE_PALETTE[12] = RGB8( 0, 36, 145 );
+	SPRITE_PALETTE[13] = RGB8( 0, 36, 109 );
+	SPRITE_PALETTE[14] = RGB8( 145, 36, 0 );
+	SPRITE_PALETTE[15] = RGB8( 255, 145, 0 );
+
+	for(int i = 0; i < 128; i++) {
+		OAM_MEM[i].attr0 = ATTR0_DISABLED;
+	}
+
+	u16* sprite_vram = (u16*)( 0x06010000); // CHARACTER/TILED MODE SO 
+	dmaCopy( shipTiles, sprite_vram, shipTilesLen );
+	OAM_MEM[0].attr0 = ATTR0_NORMAL | ATTR0_COLOR_16 | ATTR0_SQUARE | OBJ_Y(40); 
+	OAM_MEM[0].attr1 = ATTR1_SIZE_16 | OBJ_X(50);
+	OAM_MEM[0].attr2 = OBJ_CHAR(0) | OBJ_PRIORITY(0);
+
 
 	// use background 0 with tile base 0
 	//    #define TEXTBG_SIZE_256x256    BG_SIZE_0
 	//    BG_16_COLOR             =       (0<<7),         /*!< background uses 16 color tiles             */
-
 	REG_BG1CNT = ( BG_SIZE_3 | BG_16_COLOR | TILE_BASE(0) | MAP_BASE(8) );
 
 	int camera_x = 0;
 	int camera_y = 0;
+	int tick = 0;
+	int shipFrame = 0;
 	while (1) {
 		VBlankIntrWait();
 		scanKeys();
@@ -68,6 +100,15 @@ int main(void) {
 
 		if (keys & KEY_DOWN)
 			camera_y++;
+
+		tick++;
+		if( tick % 5 == 0 ) {	
+			shipFrame+=4;
+			if( shipFrame > 28 ) shipFrame = 0;
+			OAM_MEM[0].attr2 &= 0xfc00;
+			OAM_MEM[0].attr2 |= OBJ_CHAR(shipFrame);
+		}
+
 
 		REG_BG1HOFS = camera_x;
 		REG_BG1VOFS = camera_y;
---
+++ 06-scrolling_background/source/ship.h
@@ -0,0 +1,10 @@
+
+#ifndef _SHIP_H_
+#define _SHIP_H_
+
+#define shipTilesLen 1024
+extern const unsigned short shipTiles[512];
+
+
+#endif  // _SHIP_H_
+
