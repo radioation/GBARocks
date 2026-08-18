d4312e67d8b467f5787f1be643264622a7c23263
Greg Gallardo <greggallardo@gmail.com>
2026-08-17 19:06:39 -0500

    update gfx

--- GBARocks/source/ship.h
+++ GBARocks/source/ship.h
@@ -8,7 +8,7 @@
 //	+ 64 tiles Metatiled by 2x2 not compressed
 //	Total size: 32 + 2048 = 2080
 //
-//	Time-stamp: 2026-08-17, 18:06:10
+//	Time-stamp: 2026-08-17, 18:41:42
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
