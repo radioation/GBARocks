e6e19625e48343b4eaffaaddb44ace3d85ee0dd5
Greg Gallardo <greggallardo@gmail.com>
2026-08-28 16:07:10 -0500

    simple elapsed time example

--- 09-timers/NOTES.md
+++ 09-timers/NOTES.md
@@ -1,1 +1,15 @@
 # Timers
+Largely from [here](https://problemkaputt.de/gbatek.htm#gbatimers)
+
+* 4 16-bit timers
+
+IN memory at 32 bit addresses
+
+## Low 16-bits:
+  * 4000100h - `TM0CNT_L` - Timer 0 Counter/Reload (R/W)
+  * 4000104h - `TM1CNT_L` - Timer 1 Counter/Reload (R/W)
+  * 4000108h - `TM2CNT_L` - Timer 2 Counter/Reload (R/W)
+  * 400010Ch - `TM3CNT_L` - Timer 3 Counter/Reload (R/W)
+
+
+
--- 09-timers/source/main.c
+++ 09-timers/source/main.c
@@ -7,6 +7,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <gba_timers.h>
+
+// #define TIMER_ENABLE (1<<7) already in header as TIMER_START(BIT(7))
+#define TIMER_DIV_1024  3
+
 
 //---------------------------------------------------------------------------------
 // Program entry point
@@ -21,11 +26,32 @@ int main(void) {
 	irqInit();
 	irqEnable(IRQ_VBLANK);
 
+    consoleDemoInit();
 
+    //////////////////////////////////////////////////////////////
+    // INIT TIMER 2 with division by 1024
+    REG_TM2CNT_H = 0;   // stop timer
+    REG_TM2CNT_L = 0;   // reset counter
+
+    // 16.78 MHz / 1024 ~= 16384 Hz
+    REG_TM2CNT_H = TIMER_START | TIMER_DIV_1024;
+
+        
 
 	while (1) {
+        uint16_t start_time = REG_TM2CNT_L;   // lower 16 is current timer val
 
+        VBlankIntrWait();
+        uint16_t end_time = REG_TM2CNT_L;
+        uint16_t elapsed = end_time - start_time;
+
+        iprintf("\x1b[2J");
+        iprintf("start: %u end: %u\n", start_time, end_time );
+        iprintf("Ticks/frame: %u\n", elapsed);
 	}
+    
+
+
 }
 
 
