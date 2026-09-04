2d72715eb9fef9700d755e5334cd791680cc683e
Greg Gallardo <greggallardo@gmail.com>
2026-09-04 18:38:11 -0500

    working on more steps

--- GBARocks/NOTES.md
+++ GBARocks/NOTES.md
@@ -1,5 +1,6 @@
 ```bash 
  grit ship.png -gt -gB4 -Mw2 -Mh2 -pn16 -ftc
+ grit ship24.png -gt -gB4 -Mw2 -Mh2 -pn16 -ftc
  grit ufo.png -gt -gB4 -Mw2 -Mh2 -pn16 -ftc
  grit boom.png -gt -gB4 -Mw4 -Mh4 -pn16 -ftc
  grit shot.png -gt -gB4 -Mw1 -Mh1 -pn16 -ftc
---
+++ GBARocks/source/ship24.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(ship24)
+
+//======================================================================
+//
+//	ship24, 16x512@4, 
+//	+ palette 16 entries, not compressed
+//	+ 128 tiles Metatiled by 2x2 not compressed
+//	Total size: 32 + 4096 = 4128
+//
+//	Time-stamp: 2026-09-04, 18:37:18
+//	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_SHIP24_H
+#define GRIT_SHIP24_H
+
+#define ship24TilesLen 4096
+extern const unsigned int ship24Tiles[1024];
+
+#define ship24PalLen 32
+extern const unsigned short ship24Pal[16];
+
+#endif // GRIT_SHIP24_H
+
+//}}BLOCK(ship24)
